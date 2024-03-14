9611fa7849cb5eb820800b59a796cd85c41f9ea2
ze-ubuntu <1079903036@qq.com>
2024-03-14 14:54:27 +0800

    2024.03.14

---
+++ 7.二叉树/10-110.平衡二叉树.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.cn/problems/balanced-binary-tree/description/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        int getDepth(TreeNode* node) {
+            if(node == nullptr) return 0;
+        }
+        return true;
+    }
+};
