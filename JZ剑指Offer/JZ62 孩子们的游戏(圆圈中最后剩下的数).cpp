0f5de0921b71e3966550d9fdc2ff7333c1e9bb67
ze-ubuntu <1079903036@qq.com>
2024-07-05 00:00:01 +0800

    Automated commit at 2024年 07月 05日 星期五 00:00:01 CST

---
+++ JZ剑指Offer/JZ62 孩子们的游戏(圆圈中最后剩下的数).cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <list>
+using namespace std;
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x) :
+        val(x), next(NULL) {
+    }
+};
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+struct TreeLinkNode {
+    int val;
+    struct TreeLinkNode *left;
+    struct TreeLinkNode *right;
+    struct TreeLinkNode *next;
+    TreeLinkNode(int x) :val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+
+class Solution {
+public:
+    int LastRemaining_Solution(int n, int m) {
+        if(n < 1 || m < 1) return -1;
+        list<int> numbers;
+        for(int i = 0; i < n; i++){
+            numbers.push_back(i);
+        }
+        list<int>::iterator cur = numbers.begin();
+        while(numbers.size() > 1){
+            for(int i = 1; i < m; i++){
+                cur++;
+                if(cur == numbers.end()) cur = numbers.begin();
+            }
+            list<int>::iterator next = ++cur;
+            if(next == numbers.end()) next = numbers.begin();
+            numbers.erase(--cur);
+            cur = next;
+        }
+        return *cur;
+    }
+};
+
+int main()
+{
+    return 0;
+}
---
+++ JZ剑指Offer/JZ64 求1+2+3+...+n.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x) :
+        val(x), next(NULL) {
+    }
+};
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+struct TreeLinkNode {
+    int val;
+    struct TreeLinkNode *left;
+    struct TreeLinkNode *right;
+    struct TreeLinkNode *next;
+    TreeLinkNode(int x) :val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+class Solution {
+public:
+    int Sum_Solution(int n) {
+        //通过与运算判断n是否为正数，以结束递归
+        n && (n += Sum_Solution(n - 1)); 
+        return n;
+    }
+};
+
+int main()
+{
+    return 0;
+}
