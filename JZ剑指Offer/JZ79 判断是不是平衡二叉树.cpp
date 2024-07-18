584d8660c441eed217a40db424fffc7f6cd63dd4
ze-ubuntu <1079903036@qq.com>
2024-07-19 00:00:01 +0800

    Automated commit at 2024年 07月 19日 星期五 00:00:01 CST

--- JZ剑指Offer/JZ79 判断是不是平衡二叉树.cpp
+++ JZ剑指Offer/JZ79 判断是不是平衡二叉树.cpp
@@ -42,7 +42,7 @@ class Solution {
         //递归算左右子树的深度
         int left = deep(root->left);
         int right = deep(root->right);
-        //子树最大深度加上自己
+        // 它取左子树和右子树深度的最大值，并加上 1（表示当前节点），以得到当前树的深度。
         return (left > right) ? left + 1 : right + 1;
     }
     bool IsBalanced_Solution(TreeNode* pRoot) {
@@ -59,6 +59,36 @@ class Solution {
         return IsBalanced_Solution(pRoot->left) && IsBalanced_Solution(pRoot->right);
     }
 };
+
+// 自底向上
+class Solution {
+public:
+    //计算该子树深度
+    bool judge(TreeNode* root, int& depth){
+        //空节点深度为0
+        if(root == NULL){ 
+            depth = 0;
+            return true;
+        }
+        //准备计算左右子树的深度
+        int left = 0; 
+        int right = 0;
+        if(judge(root->left, left) == false || judge(root->right, right) == false)
+            return false;
+        //左子树深度减去右子树相差绝对值大于1
+        if(left - right > 1 || left - right < -1)
+            return false;
+        //子树最大深度加上自己
+        depth = (left > right) ? left + 1 : right + 1; 
+        //该节点满足要求
+        return true; 
+    }
+    bool IsBalanced_Solution(TreeNode* pRoot) {
+        int depth = 0;
+        return judge(pRoot, depth);
+    }
+};
+
 
 int main()
 {
