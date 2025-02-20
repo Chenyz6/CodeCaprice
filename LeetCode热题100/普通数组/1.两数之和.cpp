f45cf978cf7cc659d51bc993190e93d3da26b439
ze-ubuntu22.04.3 <1079903036@qq.com>
2025-02-21 00:00:02 +0800

    Automated commit at 2025年 02月 21日 星期五 00:00:02 CST

--- LeetCode热题100/普通数组/1.两数之和.cpp
+++ LeetCode热题100/普通数组/1.两数之和.cpp
@@ -1,1 +1,19 @@
 // https://leetcode.cn/problems/two-sum/description/?envType=study-plan-v2&envId=top-100-liked
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for(int i = 0; i < nums.size(); i++){
+            auto it = map.find(target - nums[i]);
+            if(it != map.end()){
+                return {i, it->second};
+            }
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
